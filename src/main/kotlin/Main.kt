f476de8e0ea93097e77a17f74febaa91a038cf34
dragonTigerChoi <yongho@koreaspacedata.com>
2025-07-02 02:58:41 +0900

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,37 @@
+package com
+
+import java.net.ServerSocket
+import java.util.concurrent.Executors
+
+fun main() {
+    println("start Kotlin WAS!")
+
+    val server = ServerSocket(8080)
+    val threadPool = Executors.newFixedThreadPool(4)
+
+    while (true) {
+        val client = server.accept()
+        threadPool.submit {
+            client.use {
+                val input = it.getInputStream().bufferedReader()
+                val output = it.getOutputStream().bufferedWriter()
+
+                val requestLine = input.readLine()
+                println("📥 Request: $requestLine")
+
+                val response = """
+                    HTTP/1.1 200 OK
+                    Content-Type: text/plain
+                    Content-Length: 20
+
+                    Hello from Kotlin WAS
+                """.trimIndent()
+
+                output.write(response)
+                output.flush()
+            }
+        }
+    }
+
+
+}
